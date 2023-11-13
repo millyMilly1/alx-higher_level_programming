a5decf654dbf6cbff9a99da7a652476ec195dfc8
millyMilly1 <chukwurahenry@gmail.com>
2023-11-13 09:09:35 -0800

    multi language

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const text1 = "c is fun";
-const text2 = "python is cool";
-const text3 = "javascript is amazing";
+const text1 = "C is fun";
+const text2 = "Python is cool";
+const text3 = "Javascript is amazing";
 
 console.log(text1);
 console.log(text2);
